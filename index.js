a29328e9caa096a80ff5710616baa891ccd6d988
mgkh286 <mmd1790@gmail.com>
2021-07-11 17:18:54 +0200

    add o project

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.use(express.static('dist'))
+
+app.get('/', (req, res)=>{
+  res.send('Hello from Express router')
+})
+
+
+app.listen(port, ()=> {
+  console.log(`Listening on port ${port}`)
+})
+
